91a8b284b382b6712a10c27943e8fb8627459bf4
trod <tobias.bruell94@gmail.com>
2021-10-28 13:43:46 +0200

    Added table for list of sets

--- src/app/sets/sets.component.ts
+++ src/app/sets/sets.component.ts
@@ -1,4 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { SetsService } from './sets.service';
+
+export interface set {
+  _id: string;
+  name: string;
+  namespace: string;
+  firstEvent: string;
+  lastEvent: string;
+  reason: string;
+  count: number;
+  scalingType: string;
+  events: [];
+}
+
 
 @Component({
   selector: 'app-sets',
@@ -6,10 +23,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sets.component.css']
 })
 export class SetsComponent implements OnInit {
+  displayedColumns: string[] = ['firstEvent','latestEvent','name', 'namespace', 'reason', 'count','scalingType']
+  dataSource: MatTableDataSource<set>;
+  setList : any;
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
+  constructor(private setsService : SetsService) {
+    this.dataSource = new MatTableDataSource();
+  }
+  ngAfterViewInit() {
+    this.loadSets();
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
   ngOnInit(): void {
   }
 
+
+  async loadSets() {
+    await this.setsService.getSets().subscribe((response) => {
+      this.setList = response;
+      console.log(response);
+      console.log(this.setList);
+      this.dataSource.data = this.setList; 
+    })
+  }
 }
---
+++ src/app/sets/sets.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SetsService {
+
+  constructor(private http: HttpClient) {}
+
+  getSets() {
+    return this.http.get('http://localhost:3000/sets');
+  }
+}
